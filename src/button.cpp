5a9e7ddcdf00169d66b6b347a89219f5de37b0e9
Noa <noalinden1@gmail.com>
2022-05-01 11:57:40 +0200

    Add button gpio to button data

--- src/button.cpp
+++ src/button.cpp
@@ -2,9 +2,10 @@
 #include "serial_print.h"
 
 void button_init(button_t *button, int8_t gpio) {
-    button->gpio = gpio;
-    pinMode(button->gpio, INPUT);
-    pinMode(button->gpio, INPUT_PULLUP);
+    button->data = 0;
+    BUTTON_GPIO_SET(button, gpio);
+    pinMode(gpio, INPUT);
+    pinMode(gpio, INPUT_PULLUP);
     BUTTON_VAL_PREV_SET(button, false);
     BUTTON_VAL_CUR_SET(button, false);
     BUTTON_VAL_TEMP_SET(button, false);
@@ -13,7 +14,7 @@ void button_init(button_t *button, int8_t gpio) {
 
 void button_update(button_t *button) {
     if (!BUTTON_CHANGE_ONGOING_GET(button)) {
-        BUTTON_VAL_TEMP_SET(button, digitalRead(button->gpio));
+        BUTTON_VAL_TEMP_SET(button, digitalRead(BUTTON_GPIO_GET(button)));
         if (BUTTON_VAL_TEMP_GET(button) != BUTTON_VAL_CUR_GET(button)) {
             // Initiate val change
             BUTTON_CHANGE_ONGOING_SET(button, true);
@@ -28,7 +29,7 @@ void button_update(button_t *button) {
         // See if val has changed or if it was noise
         if (millis() > button->time_val_change_ms + BUTTON_HOLD_MS) {
             BUTTON_CHANGE_ONGOING_SET(button, false);
-            if (digitalRead(button->gpio) != BUTTON_VAL_TEMP_GET(button)) {
+            if (digitalRead(BUTTON_GPIO_GET(button)) != BUTTON_VAL_TEMP_GET(button)) {
                 return;
             }
             BUTTON_VAL_PREV_SET(button, BUTTON_VAL_CUR_GET(button));
--- src/button.h
+++ src/button.h
@@ -10,19 +10,20 @@
 #define BUTTON_VAL_PREV_GET(button) (((button)->data >> 1) & 0b1)
 #define BUTTON_VAL_TEMP_GET(button) (((button)->data >> 2) & 0b1)
 #define BUTTON_CHANGE_ONGOING_GET(button) (((button)->data >> 3) & 0b1)
+#define BUTTON_GPIO_GET(button) (((button)->data >> 4) & 0b1111)
 
 #define BUTTON_VAL_CUR_SET(button, val) (((button)->data &= 0b11111110) |= (val<<0))
 #define BUTTON_VAL_PREV_SET(button, val) (((button)->data &= 0b11111101) |= (val<<1))
 #define BUTTON_VAL_TEMP_SET(button, val) (((button)->data &= 0b11111011) |= (val<<2))
 #define BUTTON_CHANGE_ONGOING_SET(button, val) (((button)->data &= 0b11110111) |= (val<<3))
+#define BUTTON_GPIO_SET(button, val) (((button)->data &= 0b00001111) |= (val<<4))
 
 #define BUTTON_PRESSED(button) (BUTTON_VAL_CUR_GET(button) == 0 && BUTTON_VAL_PREV_GET(button) == 1)
 
 
 // Maybe store time_val_change as uint8_t time[3]. This would only save 1 byte though and be really annoying
 typedef struct button__ {
-    uint8_t gpio;
-    uint8_t data; // bit 0 = val_cur, bit 1 = val_prev, bit 2 = val_temp, bit 3 = change_onging
+    uint8_t data; // bit 0 = val_cur, bit 1 = val_prev, bit 2 = val_temp, bit 3 = change_onging, bits 4-8 = gpio num
     uint32_t time_val_change_ms;
 } button_t;
 
